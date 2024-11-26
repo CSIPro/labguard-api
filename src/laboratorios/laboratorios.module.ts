6e1612e54617b28311df80269fc2686902183bc6
Jogibeda <117044716+Jogibeda@users.noreply.github.com>
2024-11-26 16:37:52 -0700

    vaciar

--- src/laboratorios/laboratorios.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { LaboratoriosService } from './laboratorios.service';
-import { LaboratoriosController } from './laboratorios.controller';
-
-@Module({
-  controllers: [LaboratoriosController],
-  providers: [LaboratoriosService],
-})
-export class LaboratoriosModule {}
--- src/reportes/entities/reporte.entity.ts
+++
@@ -1,8 +0,0 @@
-import { Column, DeleteDateColumn,  Entity, 
-    PrimaryGeneratedColumn, CreateDateColumn, UpdateDateColumn
- } from "typeorm";
-
-export class Reporte {
-
-
-}
