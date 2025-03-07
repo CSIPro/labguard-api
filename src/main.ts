4287e44d49d063708af0333f47aa46b04a3d5c14
Saúl Ramos <phasecross@hotmail.com>
2025-03-06 17:13:20 -0700

    feat: add cross-env, dotenv, cors

--- src/main.ts
+++ src/main.ts
@@ -1,26 +1,26 @@
-import { ValidationPipe } from "@nestjs/common";
-import { NestFactory } from "@nestjs/core";
-import { AppModule } from "./app.module";
+import { ValidationPipe } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   // Habilitar CORS con configuración específica
   app.enableCors({
-    origin: 'http://localhost:5173', // Permitir solo tu frontend local
+    origin: ['http://localhost:5173', 'https://labguard.unison.mx'], // Permitir solo tu frontend local
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     allowedHeaders: 'Content-Type, Authorization',
-    credentials: true
+    credentials: true,
   });
 
-  app.setGlobalPrefix("api/v1");
+  app.setGlobalPrefix('api/v1');
 
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
       forbidNonWhitelisted: true,
       transform: true,
-    })
+    }),
   );
 
   await app.listen(3000);
